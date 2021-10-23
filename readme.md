842cdd00045bf7ef182c0a6ba678d12f62a6bf36
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-10-23 14:45:56 +0530

    Initialize repository with easy-ui5

---
+++ readme.md
@@ -0,0 +1,7 @@
+# TimeApproval2
+
+Insert the purpose of this project and some interesting infos here
+
+## Credits
+
+This project has been generated with 💙 and [easy-ui5](https://github.com/SAP)
---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/infocus/TimeApproval2/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.infocus.TimeApproval2.controller.MainView", {});
+});
